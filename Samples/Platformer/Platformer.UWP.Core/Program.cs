b6db7a91c53c6a03193014415c5fdfe4b52c32cf
Nikos Kastellanos <nkastellanos@gmail.com>
2020-01-25 20:05:49 +0200

    Add Platformer Sample KNI projects

---
+++ Samples/Platformer/Platformer.UWP.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Platformer
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new Microsoft.Xna.Platform.GameFrameworkViewSource<PlatformerGame>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
