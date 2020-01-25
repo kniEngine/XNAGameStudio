8eedee8219c1f20e70fec6017523e07dba43dcbf
Nikos Kastellanos <nkastellanos@gmail.com>
2020-01-25 12:12:34 +0200

    Add Orientation Sample KNI projects

---
+++ Samples/Orientation/OrientationSample.UWP.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OrientationSample
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new Microsoft.Xna.Platform.GameFrameworkViewSource<OrientationSample>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
